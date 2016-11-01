776e88d6cf620a4b9fb683cba847d12984a75b65
amitOodles <amit.k@oodlestechnologies.com>
2016-11-01 17:08:48 +0530

    csv attached, cron and ajax submit

--- server.js
+++ server.js
@@ -1,44 +1,70 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var fs = require('fs');
+var csv = require('fast-csv');
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+var xoauth2 = require('xoauth2');
 
 mongoose.connect('mongodb://localhost/tima');
 
 var nodemailer = require('nodemailer');
-var transporter = nodemailer.createTransport('smtps://amitnirwal80%40gmail.com:Amitnirwal@1@smtp.gmail.com');
+// var transporter = nodemailer.createTransport('smtps://amitnirwal80%40gmail.com:Amitnirwal%401@smtp.gmail.com');
+
+// login
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        xoauth2: xoauth2.createXOAuth2Generator({
+            user: 'MailerAmit0@gmail.com',
+            clientId: '56525319787-9r4r3i07ul08sdupcesdqe06mo4ns5iv.apps.googleusercontent.com',
+            clientSecret: 'SSSpVfAqe_73lW2dU-PZSeIT',
+            refreshToken: '1/I3v6OkL5bkdbhWM3v0wqbm2xtHBre9k1mRIJwyy_D28--ounMQwSeAewRnc_15RM'
+        })
+    }
+});
 
 var CronJob = require('cron').CronJob;
 
 
 
 var mailOptions = {
-    from: '"Amit" <amitnirwal80@gmail.com>', // sender address
-    to: 'amit.k@oodlestechnologies.com', // list of receivers
+    from: '"Amit" <MailerAmit0@gmail.com>', // sender address
+    to: 'amit.k@oodlestechnologies.com,kartik.tickoo@oodlestechnologies.com', // list of receivers
     subject: 'Hello ✔', // Subject line
     text: 'Hello world 🐴', // plaintext body
-    html: '<b>Hello world 🐴</b>' // html body
+    html: '<b>Hello world 🐴</b>', // html body
+    attachments: []
+        // attachments: [{
+        //     path: '../a.pdf',
+        //     contentType: 'application/pdf'
+        // }]
 };
 
-var job = new CronJob('* * * * * *', function(){
+var job = new CronJob('* * * * * *', function() {
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: ' + info.response);
+        });
 
-	// console.log("job");
+    }, function() {
 
-	    transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        return console.log(error);
-    }
-    console.log('Message sent: ' + info.response);
-});
+        console.log("job ends");
 
-  }, function (){
-
-  	console.log("job ends");
-
-  },
-  true,"Asia/Kolkata"
+    },
+    false, "Asia/Kolkata"
 );
 
-job.start();
+// job.start();
 
 const bodyParser = require('body-parser');
 
@@ -48,63 +74,94 @@ app.use(bodyParser.json());
 // if our user.js file is at app/models/user.js
 var User = require('./models/User');
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!');
 });
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/users', function(req, res) {
+    // get all the users
+    User.find({}, function(err, users) {
+        if (err) throw err;
+
+        var date = new Date();
+
+        var fileName = date.toString().slice(0, 15);
+
+
+
+        var csvStream = csv.createWriteStream({ headers: true }),
+            writableStream = fs.createWriteStream("../" + fileName + ".csv");
+
+        writableStream.on("finish", function() {
+            mailOptions.attachments.push({ path: "../" + fileName + ".csv" });
+            transporter.sendMail(mailOptions, function(error, info) {
+                if (error) {
+                    return console.log(error);
+                }
+                console.log('Message sent: ' + info.response);
+            });
+            res.send("generated csv");
+        });
+
+        csvStream.pipe(writableStream);
+        for (var i = 0; i < users.length; i++) {
+            csvStream.write({ NAME: users[i].name, "E-MAIL": users[i].email, PHONE: users[i].mobile, REVIEW: users[i].reviews[0].calculator + " - " + users[i].reviews[0].feedback });
+        }
+        csvStream.end();
+    });
+
 });
 
 
+app.post("/mongo", function(req, res) {
 
-app.post("/mongo",function(req,res){
+    var data = req.body;
 
-	var data = req.body;
+    console.log(data);
 
-	console.log(data);
+    var user = new User({
+        name: data.name,
+        mobile: data.mobile,
+        email: data.email,
+        created_at: new Date()
+    })
 
-	var user = new User({
-		name : data.name,
-		mobile : data.mobile,
-		email : data.email,
-		created_at : new Date()
-	})
+    user.reviews.push(data.reviews);
 
-	user.reviews.push(data.reviews);
+    user.save(function(err) {
+        try {
+            if (err) throw err;
+        } catch (err) { res.send("an error occured" + err.type) }
+        res.send(user.name + "submitted a feedback");
+    })
 
-	user.save(function(err){
-		try{if(err) throw err;} catch(err){res.send("an error occured" + err.type)}
-		res.send(user.name + "submitted a feedback");
-	})
+    // var dataObj = JSON.parse(data);
 
-	// var dataObj = JSON.parse(data);
+    // console.log(dataObj);
 
-	// console.log(dataObj);
+    // console.log("checking mongo");
 
-	// console.log("checking mongo");
+    // create a new user called chris
+    // var chris = new User({
+    //   name: 'Chris',
+    //   username: 'sevilayha',
+    //   password: 'password' 
+    // });
 
-	// create a new user called chris
-// var chris = new User({
-//   name: 'Chris',
-//   username: 'sevilayha',
-//   password: 'password' 
-// });
+    // call the custom method. this will just add -dude to his name
+    // user will now be Chris-dude
+    // chris.dudify(function(err, name) {
+    //   if (err) throw err;
 
-// call the custom method. this will just add -dude to his name
-// user will now be Chris-dude
-// chris.dudify(function(err, name) {
-//   if (err) throw err;
+    //   console.log('Your new name is ' + name);
+    // });
 
-//   console.log('Your new name is ' + name);
-// });
+    // call the built-in save method to save to the database
+    // chris.save(function(err) {
+    //   if (err) throw err;
 
-// call the built-in save method to save to the database
-// chris.save(function(err) {
-//   if (err) throw err;
-
-//   console.log('User saved successfully!');
-//   res.send("saved user");
-// });
+    //   console.log('User saved successfully!');
+    //   res.send("saved user");
+    // });
 
 })
